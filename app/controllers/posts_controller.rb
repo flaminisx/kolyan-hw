56a0cd6de1b638eb4e06ae2f97d62d426799c314
Nickolas981 <kolin98111@gmail.com>
2016-11-22 22:53:19 +0200

    first app on rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,15 +10,34 @@ class PostsController < ApplicationController
   def create
     @post = Post.new post_params
     if @post.save
-      render json: @post, status: 201
+      render :show, status: 201
     else
       render plain: "Not Valid data!", status: 422
     end
   end
 
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def destroy
+    Post.find(params[:id]).delete 
+    redirect_to posts_path
+  end 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update post_params
+    redirect_to post_path(@post)
+  end
   private
 
   def post_params
     params.require(:post).permit :title, :content
   end
+
 end
